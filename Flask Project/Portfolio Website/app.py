c5119a6ee3e13914cd5a82261761d0be3fcc24eb
rahul-tk3355 <rahulthakur091996@gmail.com>
2024-10-02 02:06:05 +0530

    add new folder

---
+++ Flask Project/Portfolio Website/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/my_projects")
+def my_projects():
+    return render_template("my_projects.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
